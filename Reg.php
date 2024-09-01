8c2729948d08a121b21da9eb09fe0b93b3a0a1cf
VarunPanchbhai <119111728+VarunPanchbhai@users.noreply.github.com>
2024-09-01 11:27:19 +0530

    Create Reg.php

---
+++ Reg.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>CAB</title>
+<link rel="stylesheet" href="style3.css">
+<script src="https://kit.fontawesome.com/9f15565ed8.js" crossorigin="anonymous"></script>
+</head>
+<style>body{
+background-image: url("90.jpg");
+background-size:cover;
+}
+.ca2
+{
+border: 2px;
+background-color: rgb(18, 17, 17);
+width: 400px;
+margin:20px;
+padding: 25px;
+color:white;
+box-shadow: 0px 0px 20px rgba(255, 255, 255, 0.4)
+}
+</style>
+<nav>
+<div class="heading">
+<h4>CAB BOOKING SYSTEM</h4>
+</div>
+<ul class="nav-links">
+<li><a href="about.html">Home</a></li>
+<li><a href="home.html">About</a></li>
+<li><a href="c4.html">Cab</a></li>
+<li><a class="active" href="map.html">Contact</a></li>
+</ul>
+</nav>
+<body>
+<br> <br> <br> <br> <br>
+<br> <br> <br> <br> <br> <br>
+<center>
+<div class="ca2">
+<h1>Thanks! For contacting us</h1>
+<img src="w.jpg" width="300" height="300">
+</div>
+</center>
+</body>
+</html>
